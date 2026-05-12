62c0cbaf3b4acf67693fe6aabe14d1e86944243b
Tanisha Soni <stanisha585@gmail.com>
2026-05-12 16:15:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,11 +11,16 @@ input_news = st.text_area("Enter News Article")
 
 if st.button("Predict"):
 
-    transformed_text = vectorizer.transform([input_news])
-
-    prediction = model.predict(transformed_text)
-
-    if prediction[0] == 1:
-        st.success("Real News ✅")
+    if len(input_news) < 50:
+        st.warning("Please enter a longer news article.")
+    
     else:
-        st.error("Fake News ❌")
+
+        transformed_text = vectorizer.transform([input_news])
+
+        prediction = model.predict(transformed_text)
+
+        if prediction[0] == 1:
+            st.success("Real News ✅")
+        else:
+            st.error("Fake News ❌")
