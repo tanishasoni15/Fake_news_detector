4229caa32327d03967ee03039ac47360bde78f1d
Tanisha Soni <stanisha585@gmail.com>
2026-05-12 15:49:21 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import pickle
+
+model = pickle.load(open("fake_news_model.pkl", "rb"))
+
+vectorizer = pickle.load(open("vectorizer.pkl", "rb"))
+
+st.title("Fake News Detection")
+
+input_news = st.text_area("Enter News Article")
+
+if st.button("Predict"):
+
+    transformed_text = vectorizer.transform([input_news])
+
+    prediction = model.predict(transformed_text)
+
+    if prediction[0] == 0:
+        st.error("Fake News ❌")
+    else:
+        st.success("Real News ✅")
